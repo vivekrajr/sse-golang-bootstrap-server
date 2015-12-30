052ff06ee675c04f521e84e854b3b9368f594390
Vivek Raj <vivekrajr@icloud.com>
2015-12-31 00:32:27 +0530

    Added comments for source code

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+//Simple example for server sent events
 package main
 
 import (
@@ -7,10 +8,15 @@ import (
 	"time"
 )
 
+//The channel which gets the events
 var eventChannel chan int
 
+//When a client listens, for SSE, this is the handler function
 func HandleSSE(w http.ResponseWriter, r *http.Request) {
+
+	//Add header to allow requests from any origin
 	w.Header().Add("Access-Control-Allow-Origin", "*")
+
 	conn, err := sse.Upgrade(w, r)
 
 	if err != nil {
@@ -20,7 +26,10 @@ func HandleSSE(w http.ResponseWriter, r *http.Request) {
 
 	for {
 		select {
+
+		//Listen on eventChannel for an integer
 		case number := <-eventChannel:
+			//Write the integer received to the stream
 			conn.WriteString(fmt.Sprintf("%d", number))
 		}
 	}
@@ -29,8 +38,10 @@ func HandleSSE(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
+	//create channel for events
 	eventChannel = make(chan int)
 
+	//Start a goroutine for producing "events"
 	go producer()
 
 	http.HandleFunc("/event", HandleSSE)
@@ -38,6 +49,7 @@ func main() {
 	http.ListenAndServe(":3000", nil)
 }
 
+//Infinitely send 1 to 100 to channel with 100ms delay
 func producer() {
 	i := 1
 
